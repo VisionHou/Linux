29874d957186d00da331aa05a8ce55392a97283d
VisionHou <44837243+VisionHou@users.noreply.github.com>
2018-12-11 14:56:44 +0800

    Update cond.cc

--- cond.cc
+++ cond.cc
@@ -1,30 +1,45 @@
-#include<iostream>
-  2 #include<pthread.h>
-  3 #include<string>
-  4 #include<unistd.h>
-  5 
-  6 pthread_mutex_t lock;
-  7 pthread_cond_t cond;
-  8 using namespace std;
-  9 
- 10 void *r1(void *arg)
- 11 {
- 12     std::string t1=(char*)arg;
- 13     while(1)
- 14     {
- 15         pthread_cond_wait(&cond,&lock);
- 16         cout<<t1<<"is running"<<endl;
- 17     }
- 18 }
- 19 
- 20 void *r2(void *arg)
- 21 {
- 22     while(1)
- 23     {
- 24         pthread_cond_signal(&cond);
- 25         sleep(2);
- 26     }
- 27 }
- 28 int main()
- 29 {
- 30     pthread_mute
+#include <iostream>
+#include <string>
+#include <pthread.h>
+#include <unistd.h>
+
+pthread_mutex_t lock;
+pthread_cond_t cond;
+
+using namespace std;
+
+void *r1(void *arg)
+{
+    std::string t1 = (char*)arg;
+    while(1)
+    {
+        pthread_cond_wait(&cond, &lock);
+        cout << t1 << " is running" << endl;
+    }
+}
+
+void *r2(void *arg)
+{
+    while(1)
+    {
+        pthread_cond_signal(&cond);
+        sleep(2);
+    }
+}
+
+int main()
+{
+    pthread_mutex_init(&lock, NULL);
+    pthread_cond_init(&cond, NULL);
+
+    pthread_t t1;
+    pthread_t t2;
+    pthread_create(&t1, NULL, r1, (void *)"thread 1");
+    pthread_create(&t2, NULL, r2, (void *)"thread 2");
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    pthread_mutex_destroy(&lock);
+    pthread_cond_destroy(&cond);
+    return 0;
+}
